9f29ddfbde53d0edd489b9688af226ce3d074264
GIDRO32 <pambossable@gmail.com>
2022-10-12 10:57:46 +0300

    Add files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+public class Main
+{
+    public static class Money
+    {
+        private static float cash;//wallet balance
+        private static float card;//credit card balance
+        private static String yourCurrency;//dollars, hryvnias etc.
+        private static String YourName;
+        public Money(float i, String currency, float i2, String name)
+        {
+            cash = i;
+            card = i2;
+            yourCurrency = currency;
+            YourName = name;
+        }
+        public static float getMoneySum()
+        {
+            return cash + card;
+        }
+        public static void printFullInfo()
+        {
+            System.out.println("There are " + cash + " " + yourCurrency + " in the wallet owned by " + YourName);
+            System.out.println("There are " + card + " " + yourCurrency + " on the card owned by " + YourName);
+            System.out.println("You have " + getMoneySum() + " " + yourCurrency + " in total");
+        }
+    }
+    public static void main(String[] args)
+    {
+        Money sasha = new Money(3.64f, "dollars", 53.36f, "Oleksandr");
+        sasha.printFullInfo();
     }
 }
